ce6896658d33705faea75c7e2cb9584d5fb0c5a4
Daniel Bø Golan <danielbogolan@daniels-mbp.lan>
2021-12-20 11:02:49 +0100

    button functions

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,30 @@ import "package:flutter/material.dart";
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("Answer choosen");
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "Whats you favorite colour?",
+      "Whats your favorite animal?",
+    ];
     return MaterialApp(
-      home: Text("Hello!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App!"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text("The question: "),
+            RaisedButton(child: Text("Answer 1"), onPressed: answerQuestion),
+            RaisedButton(child: Text("Answer 2"), onPressed: answerQuestion),
+            RaisedButton(child: Text("Answer 3"), onPressed: answerQuestion),
+          ],
+        ),
+      ),
     );
   }
 }
